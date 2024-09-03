f2c582ff1a6997581a58ce5e8782ad6a8cdb8313
favoured mwange <mwangefavoured@gmail.com>
2024-09-03 22:45:51 +0300

    Practices on list comprehensions and generator expressions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # pass
+    # num_list = range(1,10,2)
+    even= [n for n in num_list if n % 2 == 0]
+    return even
 
 def make_exclamation(sentence_list):
-    pass
+    # pass
+    sentence = [sen + '!' for sen in sentence_list]
+    print(sentence)
+    return sentence
